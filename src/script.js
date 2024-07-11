dcdaa9822e409f8f6adb98ba279712a43288c7c0
makoge boris <makogeboris@gmail.com>
2024-07-11 13:08:27 +0100

    modified

--- src/script.js
+++ src/script.js
@@ -1,3 +1,41 @@
 "use strict";
 
-console.log("alakuii");
+document.addEventListener("DOMContentLoaded", function () {
+  //   const validate = new window.JustValidate('#form');
+  const validator = new window.JustValidate("#basic_form");
+
+  validator
+    .addField("#basic_name", [
+      {
+        rule: "required",
+      },
+      {
+        rule: "minLength",
+        value: 3,
+      },
+      {
+        rule: "maxLength",
+        value: 15,
+      },
+    ])
+    .addField("#basic_email", [
+      {
+        rule: "required",
+      },
+      {
+        rule: "email",
+      },
+    ])
+
+    .addField("#basic_message", [
+      {
+        rule: "required",
+      },
+      {
+        rule: "minLength",
+        value: 5,
+      },
+    ]);
+
+  location.reload();
+});
