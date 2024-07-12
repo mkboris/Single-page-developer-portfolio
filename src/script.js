731b18f334eda6a1acc058fcd8be2221f8679dab
makoge boris <makogeboris@gmail.com>
2024-07-12 14:11:18 +0100

    modified

--- src/script.js
+++ src/script.js
@@ -36,7 +36,28 @@ document.addEventListener("DOMContentLoaded", function () {
     ])
     .onSuccess(function (event) {
       event.preventDefault();
-      alert("Thanks for your message! I will get back to you soon.");
-      location.reload();
+
+      formSubmit();
+
+      // alert("Thanks for your message! I will get back to you soon.");
+      // location.reload();
     });
 });
+
+// const form = document.getElementById("form");
+// form.addEventListener("submit", formSubmit);
+
+function formSubmit(e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+
+  fetch("https://getform.io/f/bnlelkmb", {
+    method: "POST",
+    body: formData,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error));
+}
